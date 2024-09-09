f5d7a9c50a58010d06730be0246e191b0dd89414
tamutamu3 <withbw.8008@gmail.com>
2024-09-10 01:44:16 +0900

    ユーザー管理機能の作成

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください', allow_blank: true } do
+    validates :familyname
+    validates :name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カナ文字を使用してください', allow_blank: true } do
+    validates :familyname_kana
+    validates :name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
+end
