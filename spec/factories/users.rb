31ab216e451cc871277879fa1dec9cba2692cad0
tamutamu3 <withbw.8008@gmail.com>
2024-09-11 21:28:59 +0900

    テストコードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,10 +6,11 @@ FactoryBot.define do
     password_confirmation { password }
     kanji_characters = %W[\u5C71\u7530 \u9234\u6728 \u7530\u4E2D \u4F50\u85E4 \u4F0A\u85E4]
     hiragana_characters = %W[\u306F\u306A\u3053 \u3042\u3044 \u3048\u307F]
+    katakana_characters = %W[\u30A2\u30AA\u30A4 \u30E6\u30A6\u30AD \u30AB\u30A4]
     familyname            { kanji_characters.sample }
     name                  { hiragana_characters.sample }
-    familyname_kana       { Faker::Name.last_name.upcase.tr('A-Z', 'ア-ン') }
-    name_kana             { Faker::Name.first_name.upcase.tr('A-Z', 'ア-ン') }
+    familyname_kana       { katakana_characters.sample }
+    name_kana             { katakana_characters.sample }
     birthday              { Faker::Date.birthday }
   end
 end
