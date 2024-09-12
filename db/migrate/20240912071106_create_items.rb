bbf0d6d9c12485b000614109a89507266647b15a
tamutamu3 <withbw.8008@gmail.com>
2024-09-12 16:21:33 +0900

    カラムの作成

--- db/migrate/20240912071106_create_items.rb
+++ db/migrate/20240912071106_create_items.rb
@@ -3,6 +3,14 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table :items do |t|
 
       t.timestamps
+      t.string :item_name, null: false
+      t.text :explain, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :fee_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :prepare_id, null: false
+      t.integer :price, null: false
     end
   end
 end
