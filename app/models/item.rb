c2f653eb601de041a66de4a846178c5a4fcae7a9
tamutamu3 <withbw.8008@gmail.com>
2024-09-12 16:30:01 +0900

    itemモデルのバリデーションとアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :prepare_id, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
 end
