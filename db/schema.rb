f87f32907d754552ff44e17b8ca53cb73361c704
tamutamu3 <withbw.8008@gmail.com>
2024-09-16 10:36:17 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_05_152212) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_12_071106) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "item_name", null: false
+    t.text "explain", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "prepare_id", null: false
+    t.integer "price", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
