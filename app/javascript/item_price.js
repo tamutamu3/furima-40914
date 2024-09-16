1c0ed665c6679669070761fd1195d238e53c94a7
tamutamu3 <withbw.8008@gmail.com>
2024-09-16 19:27:43 +0900

    販売手数料や販売利益の計算結果の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('turbo:load', () => {
+  const price = () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+      const profitPrice = document.getElementById("profit");
+      profitPrice.innerHTML = Math.floor(inputValue*0.9);
+    })
+  };
+  window.addEventListener("turbo:load", price);
+  window.addEventListener("turbo:render", price)
+});
