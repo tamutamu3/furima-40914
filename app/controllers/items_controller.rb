09dc83c5b440b3bf31e1a2ea781c3ebec38a3003
tamutamu3 <withbw.8008@gmail.com>
2024-09-24 15:01:08 +0900

    updateアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    if @item.save
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
