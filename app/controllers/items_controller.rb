43d502d062b45797ade38ae03e0966a530eb415d
tamutamu3 <withbw.8008@gmail.com>
2024-09-26 00:53:08 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update, :destory]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :edit, :destory]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @item = Item.order('created_at DESC')
@@ -34,16 +34,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destory
-    @item.destory
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
     redirect_to root_path
   end
 
   private
-
-  def move_to_index
-    redirect_to root_path unless current_user == @item.user
-  end
 
   def item_params
     params.require(:item).permit(:item_name, :explain, :category_id, :condition_id, :fee_id, :prefecture_id, :prepare_id, :price,
@@ -52,5 +49,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
